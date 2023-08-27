43ae54209f3a504d025923603056df181e5e45d5
BartoszJO <bartoszjolszewski@hotmail.com>
2023-08-27 11:41:13 +0200

    Added Features

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        for (int i = 1; i < 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("To jest kolejna modyfikacja");
     }
 }
